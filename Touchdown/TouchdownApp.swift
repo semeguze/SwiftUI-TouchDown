0f0fb7a05fc7df4776ea180a636e0ab080bf4919
Sebastian Mesa <semeguze@hotmail.com>
2022-06-05 10:03:36 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Sebastian Mesa on 5/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
